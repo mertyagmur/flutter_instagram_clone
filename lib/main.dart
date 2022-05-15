3127e4735afae402bcf51e6329e9cbd9af38b8a4
Mert Yağmur <mert.yagmurcs@gmail.com>
2022-05-15 18:14:33 +0300

    Setup Web for Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() async{
-  
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyD9SZgfhMPNK_OcEZbMZwOpTS9GSYivrBw',
+          appId: '1:653823011051:web:c5ccb9531ac534c204b6ae',
+          messagingSenderId: '653823011051',
+          projectId: 'flutter-instagram-clone-19459',
+          storageBucket: 'flutter-instagram-clone-19459.appspot.com'),
+    );
+  }
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
