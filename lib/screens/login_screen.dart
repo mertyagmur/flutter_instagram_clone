431cd0add361641612539df17cf702e528695b7c
Mert Yağmur <mert.yagmurcs@gmail.com>
2022-05-16 21:18:49 +0300

    Create Login UI

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/widgets/text_field_input.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Container(),
+                  flex: 2,
+                ),
+                // svg image
+                SvgPicture.asset(
+                  "assets/ic_instagram.svg",
+                  color: primaryColor,
+                  height: 64,
+                ),
+                const SizedBox(height: 64),
+                // email text field
+                TextFieldInput(
+                  textEditingController: _emailController,
+                  hintText: "Enter your email",
+                  textInputType: TextInputType.emailAddress,
+                ),
+                const SizedBox(height: 24),
+                // password text field
+                TextFieldInput(
+                  textEditingController: _passwordController,
+                  hintText: "Enter your password",
+                  textInputType: TextInputType.text,
+                  isPass: true,
+                ),
+                const SizedBox(height: 24),
+                // login button
+                InkWell(
+                  child: Container(
+                    child: const Text("Log in"),
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    decoration: const ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4),
+                        ),
+                      ),
+                      color: blueColor,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                Flexible(
+                  child: Container(),
+                  flex: 2,
+                ),
+                // transitioning to signup
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: const Text("Don't have an account?"),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: (() {
+                        
+                      }),
+                      child: Container(
+                        child: const Text(
+                          " Sign Up",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ),
+    );
+  }
+}
